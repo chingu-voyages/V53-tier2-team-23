4d1ecda45ef61ad6e22ab4600aa9e86ecaf095d2
Thea Win <mstheawin@gmail.com>
2025-01-14 14:36:08 +0400

    updated file structure for client
    to follow this structure for the project

---
+++ client/src/components/component.jsx
@@ -0,0 +1,10 @@
+const ExampleComponent = () => {
+  return (
+    <div className="p-4 bg-gray-100 rounded shadow">
+      <h1 className="text-xl font-bold">Example Component</h1>
+      <p>This is an example of a reusable UI component.</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
