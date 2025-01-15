2e95a01fc86d4f61bf160811a3ac00e665777ad4
leonalkalai <koundouras@gmail.com>
2025-01-15 13:33:46 +0200

    test get

--- api/src/login.js
+++ api/src/login.js
@@ -8,27 +8,30 @@ const manager = {
 
 export default function handler(request, response) {
   // POST METHOD
-  if (request.method === "POST") {
-    const { username, password } = request.body;
+  // if (request.method === "POST") {
+  //   const { username, password } = request.body;
 
-    if (username === manager.username && password === manager.password) {
-      const token = jwt.sign(
-        { username: manager.username },
-        process.env.JWT_SECRET
-      );
+  //   if (username === manager.username && password === manager.password) {
+  //     const token = jwt.sign(
+  //       { username: manager.username },
+  //       process.env.JWT_SECRET
+  //     );
 
-      response.setHeader("auth-token", token);
-      response.setHeader("Content-Type", "application/json");
+  //     //response.setHeader("auth-token", token);
+  //     response.setHeader("Authorization", `Bearer ${token}`);
 
-      response.status(200).json({ token });
-    } else {
-      response.status(401).json({ message: "Credentials don't match" });
-    }
-  }
+  //     response.setHeader("Content-Type", "application/json");
+
+  //     response.status(200).json({ token });
+  //   } else {
+  //     response.status(401).json({ message: "Credentials don't match" });
+  //   }
+  // }
 
   // GET METHOD
   if (request.method === "GET") {
-    const token = request.headers["auth-token"];
+    const token = request.headers.authorization?.split(" ")[1];
+    //const token = request.headers["auth-token"];
     if (!token) {
       return response.status(401).json({ message: "Token not found" });
     }
