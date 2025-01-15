57620a1cc4902f0cf6ae402fc3f55b13ab91e248
leonalkalai <koundouras@gmail.com>
2025-01-15 12:49:36 +0200

    created serverless login

--- api/src/login.js
+++ api/src/login.js
@@ -1,6 +1,49 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Serverless!" }),
-  };
+import jwt from "jsonwebtoken";
+
+// CREDENTIALS
+const manager = {
+  username: "manager",
+  password: "manager",
 };
+
+export default function handler(request, response) {
+  // POST METHOD
+  if (request.method === "POST") {
+    const { username, password } = request.body;
+
+    if (username === manager.username && password === manager.password) {
+      const token = jwt.sign(
+        { username: manager.username },
+        `${process.env.JWT_SECRET}`
+      );
+
+      response.setHeader("Authorization", `${token}`);
+      response.setHeader("Content-Type", "application/json");
+
+      response.status(200).json({ token });
+    } else {
+      response.status(401).json({ message: "Credentials don't match" });
+    }
+  }
+
+  // GET METHOD
+  if (request.method === "GET") {
+    const token = request.headers.authorization;
+    if (!token) {
+      return response.status(401).json({ message: "Token not found" });
+    }
+
+    try {
+      const verified = jwt.verify(token, `${process.env.JWT_SECRET}`);
+      return response
+        .status(200)
+        .json({ message: "Manager content", manager: verified.username });
+    } catch (error) {
+      return response.status(401).json({ message: "Token not accepted" });
+    }
+  }
+
+  if (request.method !== "GET" && request.method !== "POST") {
+    return response.status(405).json({ message: "Method Not Allowed" });
+  }
+}
