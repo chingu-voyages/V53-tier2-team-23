3e7dd52e3aa3388aa72fbc9d6bedd66ae8525c03
leonalkalai <koundouras@gmail.com>
2025-01-15 13:52:48 +0200

    test

--- api/src/login.js
+++ api/src/login.js
@@ -8,55 +8,55 @@ const manager = {
 
 export default function handler(request, response) {
   // POST METHOD
-  // if (request.method === "POST") {
-  //   const { username, password } = request.body;
+  if (request.method === "POST") {
+    const { username, password } = request.body;
 
-  //   if (username === manager.username && password === manager.password) {
-  //     const token = jwt.sign(
-  //       { username: manager.username },
-  //       process.env.JWT_SECRET
-  //     );
+    if (username === manager.username && password === manager.password) {
+      const token = jwt.sign(
+        { username: manager.username },
+        process.env.JWT_SECRET
+      );
 
-  //     //response.setHeader("auth-token", token);
-  //     response.setHeader("Authorization", `Bearer ${token}`);
+      //response.setHeader("auth-token", token);
+      response.setHeader("Authorization", `Bearer ${token}`);
 
-  //     response.setHeader("Content-Type", "application/json");
+      response.setHeader("Content-Type", "application/json");
 
-  //     response.status(200).json({ token });
-  //   } else {
-  //     response.status(401).json({ message: "Credentials don't match" });
-  //   }
-  // }
-
-  // GET METHOD
-  if (request.method === "GET") {
-    const token = request.headers.authorization?.split(" ")[1];
-    //const token = request.headers["auth-token"];
-    if (!token) {
-      return response.status(401).json({ message: "Token not found" });
-    }
-
-    try {
-      const verified = jwt.verify(token, process.env.JWT_SECRET);
-
-      const responseBody = JSON.stringify(verified);
-
-      return {
-        statusCode: 200,
-        body: responseBody,
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "Access-Control-Allow-Origin": "*",
-        },
-      };
-
-      // return response
-      //   .status(200)
-      //   .json({ message: "Manager content", manager: verified.username });
-    } catch (error) {
-      return response.status(401).json({ message: "Token not accepted" });
+      response.status(200).json({ token });
+    } else {
+      response.status(401).json({ message: "Credentials don't match" });
     }
   }
+
+  // GET METHOD
+  // if (request.method === "GET") {
+  //   const token = request.headers.authorization?.split(" ")[1];
+  //   //const token = request.headers["auth-token"];
+  //   if (!token) {
+  //     return response.status(401).json({ message: "Token not found" });
+  //   }
+
+  //   try {
+  //     const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+  //     const responseBody = JSON.stringify(verified);
+
+  //     return {
+  //       statusCode: 200,
+  //       body: responseBody,
+  //       headers: {
+  //         "Content-Type": "application/json; charset=utf-8",
+  //         "Access-Control-Allow-Origin": "*",
+  //       },
+  //     };
+
+  //     // return response
+  //     //   .status(200)
+  //     //   .json({ message: "Manager content", manager: verified.username });
+  //   } catch (error) {
+  //     return response.status(401).json({ message: "Token not accepted" });
+  //   }
+  // }
 
   return response.status(405).json({ message: "Method Not Allowed" });
 }
