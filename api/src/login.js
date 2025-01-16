7b1f7ddda11a9554c8c61de2c18b652f92a6f0bc
leonalkalai <koundouras@gmail.com>
2025-01-16 19:47:33 +0200

    process.env. added

--- api/src/login.js
+++ api/src/login.js
@@ -2,8 +2,8 @@ const jwt = require("jsonwebtoken");
 
 // CREDENTIALS
 const manager = {
-  username: REACT_APP_MANAGER_USERNAME,
-  password: REACT_APP_MANAGER_PASSWORD,
+  username: process.env.REACT_APP_MANAGER_USERNAME,
+  password: process.env.REACT_APP_MANAGER_PASSWORD,
 };
 
 exports.handler = async (event) => {
