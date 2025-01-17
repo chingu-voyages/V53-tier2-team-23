8d5618136fd97b5bb661503a61b9a31925f84c0e
Thea Win <mstheawin@gmail.com>
2025-01-17 13:06:26 +0400

    created model for employee, dish and allergen

---
+++ api/src/models/employee.models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+  employeeId: {
+    type: Number,
+    required: true,
+    uniqure: true,
+  },
+  employeeName: {
+    type: String,
+    required: true,
+  },
+  allergies: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Allergen',
+    },
+  ],
+  dietaryRestrictions: [
+    {
+      type: String,
+      enum: [
+        'Lactose-free',
+        'Gluten-free',
+        'Vegetarian',
+        'Vegan',
+        'Diabetes-friendly',
+      ],
+      required: false,
+    },
+  ],
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
