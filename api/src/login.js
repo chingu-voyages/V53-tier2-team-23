73a759d5524fcc7ae082e4493ee417084097fe63
leonalkalai <koundouras@gmail.com>
2025-01-18 10:14:42 +0200

    changed to single quotes and renamed app.js to loginForm.js

--- api/src/login.js
+++ api/src/login.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 // CREDENTIALS ✅
 const manager = {
@@ -16,20 +16,20 @@ exports.handler = async (event) => {
   };
 
   // Handle OPTIONS preflight request
-  if (request.method === "OPTIONS") {
+  if (request.method === 'OPTIONS') {
     return {
       statusCode: 204,
       headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },
     };
   }
 
   //POST METHOD for user login
 
-  if (request.method === "POST") {
+  if (request.method === 'POST') {
     const { username, password } = request.body;
 
     if (username === manager.username && password === manager.password) {
@@ -49,33 +49,33 @@ exports.handler = async (event) => {
         statusCode: 200,
         body: JSON.stringify({ userData }),
         headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
         },
       };
     } else {
       return {
         statusCode: 401,
-        body: JSON.stringify({ message: "Credentials don't match" }),
+        body: JSON.stringify({ message: 'Credentials don't match' }),
         headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
         },
       };
     }
   }
 
   // GET METHOD verify the JWT token sent by the client
-  if (request.method === "GET") {
-    const token = request.headers.authorization?.replace("Bearer ", "");
+  if (request.method === 'GET') {
+    const token = request.headers.authorization?.replace('Bearer ', '');
 
     if (!token) {
       return {
         statusCode: 401,
-        body: JSON.stringify({ message: "Token not found" }),
+        body: JSON.stringify({ message: 'Token not found' }),
         headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
         },
       };
     }
@@ -91,17 +91,17 @@ exports.handler = async (event) => {
         statusCode: 200,
         body: responseBody,
         headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "Access-Control-Allow-Origin": "*",
+          'Content-Type': 'application/json; charset=utf-8',
+          'Access-Control-Allow-Origin': '*',
         },
       };
     } catch (error) {
       return {
         statusCode: 401,
-        body: JSON.stringify({ message: "Token not accepted" }),
+        body: JSON.stringify({ message: 'Token not accepted' }),
         headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
         },
       };
     }
@@ -109,10 +109,10 @@ exports.handler = async (event) => {
 
   return {
     statusCode: 405,
-    body: JSON.stringify({ message: "Method Not Allowed" }),
+    body: JSON.stringify({ message: 'Method Not Allowed' }),
     headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
     },
   };
 };
