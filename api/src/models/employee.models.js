2466a25f82ad288419c62ca013e88a571d0aa93b
Thea Win <mstheawin@gmail.com>
2025-01-19 16:11:57 +0400

    added example of the menu for easier ref

--- api/src/models/employee.models.js
+++ api/src/models/employee.models.js
@@ -45,4 +45,39 @@ const employeeSchema = new Schema({
   ],
 });
 
+// example of the menu
+/* {
+  "weekStartDate": "2025-01-22",
+  "days": [
+    {
+      "date": "2025-01-22",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123456')" // References a dish
+    },
+    {
+      "date": "2025-01-23",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123457')"
+    },
+    {
+      "date": "2025-01-24",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123458')"
+    },
+    {
+      "date": "2025-01-25",
+      "dish": null // No dish assigned (Day Off)
+    },
+    {
+      "date": "2025-01-26",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123459')"
+    },
+    {
+      "date": "2025-01-27",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123460')"
+    },
+    {
+      "date": "2025-01-28",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123461')"
+    }
+  ]
+} */
+
 module.exports = mongoose.model('Employee', employeeSchema);
