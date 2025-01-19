3e0f73f6e45f25e4772f723d0cd5eabba9e50a67
leonalkalai <koundouras@gmail.com>
2025-01-19 11:57:04 +0200

    added employees and allergies

--- api/src/employees.js
+++ api/src/employees.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Dish = require('./models/dish.model'); // Import Dish model
+const employee = require('./models/employee.model'); // Import employee model
 const getDb = require('./db_config/database.config.js'); // import getDatabase database from connection
 const { ObjectId } = require('mongodb'); // import ObjectId method to convert the _id field value to string [ https://www.mongodb.com/docs/manual/reference/method/ObjectId/ ]
 
@@ -21,17 +21,17 @@ const handler = async (event, context) => {
     };
   }
 
-  if (httpMethod === 'GET' && path.endsWith('/dishes')) {
+  if (httpMethod === 'GET' && path.endsWith('/employees')) {
     try {
-      const dishes = await getData('dishes');
+      const employees = await getData('employees');
       return {
         statusCode: 200,
         headers, // Include the headers in the response
         body: JSON.stringify({
           success: true,
           data: {
-            dishes: dishes,
-            dishesLength: dishes.length,
+            employees: employees,
+            employeesLength: employees.length,
           },
         }),
       };
@@ -49,18 +49,18 @@ const handler = async (event, context) => {
     }
   }
 
-  if (httpMethod === 'GET' && path.includes('/dishes/')) {
-    const dishId = path.split('/').pop(); // Extract dish id from path
+  if (httpMethod === 'GET' && path.includes('/employees/')) {
+    const employeeId = path.split('/').pop(); // Extract employee id from path
 
     try {
-      const dish = await getDish(dishId, 'dishes');
-      if (!dishId || !ObjectId.isValid(dishId)) {
+      const employee = await getEmployee(employeeId, 'employees');
+      if (!employeeId || !ObjectId.isValid(employeeId)) {
         // check if valid mongodb id
         // https://www.geeksforgeeks.org/how-to-check-if-a-string-is-valid-mongodb-objectid-in-node-js/
         return {
           statusCode: 404,
           headers,
-          body: JSON.stringify({ message: 'Dish not found' }),
+          body: JSON.stringify({ message: 'employee not found' }),
         };
       }
       return {
@@ -68,9 +68,37 @@ const handler = async (event, context) => {
         headers, // Include the headers in the response
         body: JSON.stringify({
           success: true,
-          dishId: dishId,
-          data: dish,
+          employeeId: employeeId,
+          data: employee,
         }),
+      };
+    } catch (error) {
+      console.error('Error:', error);
+      return {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify({
+          success: false,
+          message: 'An internal server error occurred.',
+          error: error.message, // Include error details for debugging
+        }),
+      };
+    }
+  }
+
+  if (httpMethod === 'POST' && path.startsWith('/employees/create')) {
+    // Set headers for the response
+    const headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    };
+
+    try {
+      const employee = await createEmployee(body);
+      return {
+        statusCode: 200,
+        headers, // Include the headers in the response
+        body: JSON.stringify({ success: true, data: employee.data }),
       };
     } catch (error) {
       console.error('Error:', error);
@@ -96,31 +124,58 @@ const handler = async (event, context) => {
   };
 };
 
-// GET request handler for all dishes
+// GET request handler for all employees
 async function getData(collectionValue) {
   // const db = await getDb();
 
   // const collection = await db.collection(collectionValue);
   // const data = await collection.find({}).toArray();
 
-  const data = await Dish.find({}); // Fetch all dishes using Mongoose
+  const data = await employee.find({}); // Fetch all employees using Mongoose
 
   // Debugging output to check the fetched data
-  // console.log('Fetched dishes:', data);
+  // console.log('Fetched employees:', data);
 
   return data;
 }
 
-// GET request handler for dish with id
-async function getDish(dishId, collectionValue) {
-  const db = await getDb();
-  const collection = await db.collection(collectionValue);
-  const query = { _id: new ObjectId(dishId) }; // set new query dish id based on entry in the database having the _id field
+// GET request handler for employee with id
+async function getEmployee(employeeId, collectionValue) {
+  // const db = await getDb();
+  // const collection = await db.collection(collectionValue);
+  // const query = { _id: new ObjectId(employeeId) }; // set new query employee id based on entry in the database having the _id field
   /* [ https://www.mongodb.com/docs/manual/reference/method/db.collection.findOne/ ]
     findOne(query) looks for a single document in the collection that matches the criteria in query (_id).
    */
-  const dish = await collection.findOne(query);
-  return dish;
+  const employee = await collection.findOne(query);
+  return employee;
+}
+
+// POST request handler to create project
+
+async function createEmployee(body) {
+  try {
+    // Parse the body
+    const reqbody = JSON.parse(body); // Parse JSON string into an object
+    const newEmployee = new Employee(reqBody); // create an new employee from model
+    const employees_db = await getdb();
+    const collection = await projects_db.collection('employees');
+
+    const employee = await collection.insertOne(newEmployee);
+
+    newEmployee._id = employee.insertedId;
+
+    // Return the new project along with its MongoDB _id
+    return {
+      success: true,
+      data: newEmployee,
+    };
+  } catch (error) {
+    console.error('Error adding new project:', error);
+
+    // Return error response
+    throw new Error('Error adding new project');
+  }
 }
 
 module.exports.handler = handler;
---
+++ api/src/models/employee.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * Employee Schema to represent employee data.
+ * @typedef {Object} Employee
+ * @property {string} employeeId - The unique identifier for the employee.
+ * @property {string} employeeName - The name of the employee.
+ * @property {Array<ObjectId>} allergies - Array of ObjectIds referencing allergens.
+ * @property {Array<string>} dietaryRestrictions - Array of dietary restrictions.
+ */
+const employeeSchema = new Schema({
+  employeeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  employeeName: {
+    type: String,
+    required: true,
+  },
+  allergies: [
+    {
+      type: Schema.Types.ObjectId, // Represents a MongoDB ObjectId, used for referencing other documents
+      ref: 'Allergen',
+      required: false,
+    },
+  ],
+  dietaryRestrictions: [
+    {
+      type: String,
+      enum: [
+        'vegetarian',
+        'vegan',
+        'pescatarian',
+        'keto',
+        'paleo',
+        'glutenFree',
+        'standard',
+        'lactose-free',
+        'diabetes-friendly',
+      ], // only take a specific set of values
+      required: false,
+    },
+  ],
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
