9416b0bb40ad2da2a2a6be09a68420af25046881
Thea Win <mstheawin@gmail.com>
2025-01-21 16:32:47 +0400

    added example of the res for menu

--- api/src/models/menu.models.js
+++ api/src/models/menu.models.js
@@ -15,10 +15,45 @@ const menuSchema = new Schema({
       dish: {
         type: Schema.Types.ObjectId, // Reference to the dishScehma
         ref: 'Dish',
-        required: true,
+        default: null, // Allow null for "Day Off"
       },
     },
   ],
 });
+
+// example of the menu
+/* {
+  "weekStartDate": "2025-01-22",
+  "days": [
+    {
+      "date": "2025-01-22",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123456')" // References a dish
+    },
+    {
+      "date": "2025-01-23",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123457')"
+    },
+    {
+      "date": "2025-01-24",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123458')"
+    },
+    {
+      "date": "2025-01-25",
+      "dish": null // No dish assigned (Day Off)
+    },
+    {
+      "date": "2025-01-26",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123459')"
+    },
+    {
+      "date": "2025-01-27",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123460')"
+    },
+    {
+      "date": "2025-01-28",
+      "dish": "ObjectId('64a76d1e2f8b2c39fc123461')"
+    }
+  ]
+} */
 
 module.exports = mongoose.model('Menu', menuSchema);
