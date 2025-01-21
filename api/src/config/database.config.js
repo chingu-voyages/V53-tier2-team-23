d3c6c2432e59ba0366be4b034aec7ff693fb68fe
Thea Win <mstheawin@gmail.com>
2025-01-21 16:32:12 +0400

    to convert for serverless architecture

--- api/src/config/database.config.js
+++ api/src/config/database.config.js
@@ -1,20 +1,35 @@
 const mongoose = require('mongoose');
 
 // using dotenv just for local testing as I don't have access to netlify
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+let client = null; // Cache for database connection
 
 const connectDatabase = async () => {
+  if (client) {
+    return client; // Return cached connection
+  }
+
   try {
-    // awaiting the connection attempt to MongoDB
-    await mongoose.connect(process.env.MONGO_URI, {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: 'eato_database',
+      autoIndex: false,
+      bufferCommands: false, // Disable Mongoose buffering for serverless
+      keepAlive: true, // Maintain the connection in serverless
+      // for better performace in serverless environments
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+
+    console.log('Connected to DB:', mongoose.connection.db.databaseName);
+
+    client = connection; // Cache the Mongoose connection
+    console.log('MongoDB connected succesfully');
+    return client;
   } catch (error) {
-    console.error(error.message);
-    process.exit(1); // Exit the process if connection fail
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
   }
 };
 
