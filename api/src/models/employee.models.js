8497642ca0bb1a63205d1f778ea32a88d647ffdf
Thea Win <mstheawin@gmail.com>
2025-01-21 16:33:09 +0400

    converted the allergies to a pre-determined list

--- api/src/models/employee.models.js
+++ api/src/models/employee.models.js
@@ -1,14 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-/**
- * Employee Schema to represent employee data.
- * @typedef {Object} Employee
- * @property {string} employeeId - The unique identifier for the employee.
- * @property {string} employeeName - The name of the employee.
- * @property {Array<ObjectId>} allergies - Array of ObjectIds referencing allergens.
- * @property {Array<string>} dietaryRestrictions - Array of dietary restrictions.
- */
+const allergen = [
+  'gluten',
+  'dairy',
+  'eggs',
+  'seafood',
+  'soy',
+  'tree nuts',
+  'peanuts',
+  'legumes',
+  'sesame seeds',
+  'corn',
+  'mustard',
+  'allium',
+  'coconut',
+  'fruits',
+];
+
+const diet = [
+  'vegetarian',
+  'vegan',
+  'pescatarian',
+  'keto',
+  'paleo',
+  'glutenFree',
+  'standard',
+  'lactose-free',
+  'diabetes-friendly',
+];
+
 const employeeSchema = new Schema({
   employeeId: {
     type: String,
@@ -19,65 +40,28 @@ const employeeSchema = new Schema({
     type: String,
     required: true,
   },
-  allergies: [
+  // shelved for Should Have, Nice To Have feature of giving user the option to add more allergies
+  /*   allergies: [
     {
       type: Schema.Types.ObjectId, // Represents a MongoDB ObjectId, used for referencing other documents
       ref: 'Allergen',
+      required: false,
+    },
+  ], */
+  allergies: [
+    {
+      type: String, // Store the allergy name as a string
+      enum: allergen, // Restrict values to the predefined list
       required: false,
     },
   ],
   dietaryRestrictions: [
     {
       type: String,
-      enum: [
-        'vegetarian',
-        'vegan',
-        'pescatarian',
-        'keto',
-        'paleo',
-        'glutenFree',
-        'standard',
-        'lactose-free',
-        'diabetes-friendly',
-      ], // only take a specific set of values
+      enum: diet, // only take a specific set of values
       required: false,
     },
   ],
 });
-
-// example of the menu
-/* {
-  "weekStartDate": "2025-01-22",
-  "days": [
-    {
-      "date": "2025-01-22",
-      "dish": "ObjectId('64a76d1e2f8b2c39fc123456')" // References a dish
-    },
-    {
-      "date": "2025-01-23",
-      "dish": "ObjectId('64a76d1e2f8b2c39fc123457')"
-    },
-    {
-      "date": "2025-01-24",
-      "dish": "ObjectId('64a76d1e2f8b2c39fc123458')"
-    },
-    {
-      "date": "2025-01-25",
-      "dish": null // No dish assigned (Day Off)
-    },
-    {
-      "date": "2025-01-26",
-      "dish": "ObjectId('64a76d1e2f8b2c39fc123459')"
-    },
-    {
-      "date": "2025-01-27",
-      "dish": "ObjectId('64a76d1e2f8b2c39fc123460')"
-    },
-    {
-      "date": "2025-01-28",
-      "dish": "ObjectId('64a76d1e2f8b2c39fc123461')"
-    }
-  ]
-} */
 
 module.exports = mongoose.model('Employee', employeeSchema);
