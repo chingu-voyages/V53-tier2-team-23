60f52d6aa01cdac83d5a50f9def9e2e42b77a70f
leonalkalai <koundouras@gmail.com>
2025-01-21 20:06:35 +0200

    test

--- api/src/employees.js
+++ api/src/employees.js
@@ -216,13 +216,14 @@ async function getEmployees() {
     const db = await getDb(); // Get the database connection
 
     // Fetch all employees using Mongoose
-    const foundEmployees = await Employee.find({}).populate('allergies'); // populate allergies
-    // .lean(); //faster query
+    const employees = await Employee.find({})
+      .populate('allergies') // populate allergies
+      .lean(); //faster query
 
-    const employees = foundEmployees.map((employee) => ({
-      ...employee,
-      allergies: employee.allergies || [], // if array allergies empty
-    }));
+    // const employees = foundEmployees.map((employee) => ({
+    //   ...employee,
+    //   allergies: employee.allergies || [], // if array allergies empty
+    // }));
 
     console.log(employees);
     const employeesNumber = await Employee.countDocuments(); // Get total employees
