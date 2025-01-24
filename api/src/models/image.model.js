224a8ffeee3115737ab7b23103c91ab5872f2bca
leonalkalai <koundouras@gmail.com>
2025-01-24 18:56:14 +0200

    fix

--- api/src/models/image.model.js
+++ api/src/models/image.model.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
-const imageUrlSchema = new mongoose.Schema({
-  url: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
+const imageUrlSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
   },
   { collection: 'imagesurl' }
-});
+);
 
 module.exports = mongoose.model('Image', imageUrlSchema);
