066a93c09dddc35c96577459dadc497deb32b562
Thea Win <mstheawin@gmail.com>
2025-01-26 16:44:28 +0400

    header component for mobile

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { CgProfile } from 'react-icons/cg'; // placeholder for profile icon
+import Logo from '../../assets/logo5.svg';
 
 function NavBar() {
   return (
-    <nav className='flex gap-16 justify-between p-4 md:px-16 lg:px-32'>
+    <nav className='flex gap-16 justify-between p-2 pt-3.5 md:px-16 lg:px-32'>
       <div className='flex gap-2'>
-        <img
-          src='https://fakeimg.pl/40x40/cccccc/909090'
-          alt='logo placeholder'
-        />
-        <div className='pt-2'>Eato</div>
+        <img src={Logo} alt='MenuHelp logo' className='w-15 h-15' />
+        <div className='pt-4 pl-3.5 font-bold text-[19px] text-primary'>
+          MenuHelp
+        </div>
       </div>
       <div className='items-center gap-8 hidden sm:flex'>
         <ul className='flex gap-8'>
@@ -18,8 +18,8 @@ function NavBar() {
           <li>Allergies</li>
         </ul>
       </div>
-      <div className='flex sm:hidden'>
-        <CgProfile size={40} />
+      <div className='relative sm:hidden '>
+        <CgProfile size={40} className='absolute top-3 right-8 text-primary' />
       </div>
     </nav>
   );
