bb859b31fcfa6619ff583c2bbe22aad20d49cdad
leonalkalai <koundouras@gmail.com>
2025-01-27 11:08:55 +0200

    dishes

--- api/src/dishes.js
+++ api/src/dishes.js
@@ -1,8 +1,21 @@
+// const mongoosee = require('mongoose');
+// const Dishes = require('../models/dishes.models');
+// const connectDatabase = require('../config/database.config');
+// const authenticate = require('../functions/authMiddleware');
 const mongoose = require('mongoose');
 const getDb = require('./db_config/database.config.js'); // import getDatabase database from connection
 const Allergen = require('./models/allergen.model'); // Import the Allergen model
+const Employee = require('./models/employee.model'); // Import employee model
+const Image = require('./models/image.model');
 const Dish = require('./models/dish.model'); // Import dish model
-const { ObjectId } = require('mongodb'); // import ObjectId method to convert the _id field value to string [ https://www.mongodb.com/docs/manual/reference/method/ObjectId/ ]
+
+const handleError = (error, method) => {
+  console.error(`Error ${method} employee: `, error);
+  return {
+    statusCode: 500,
+    body: JSON.stringify({ error: error.message }),
+  };
+};
 
 const headers = {
   'Access-Control-Allow-Origin': '*',
@@ -10,8 +23,7 @@ const headers = {
   'Access-Control-Allow-Headers': 'Content-Type',
 };
 
-const handler = async (event, context) => {
-  //async function handler(event, context) {
+async function handler(event, context) {
   const { httpMethod, path, queryStringParameters, body } = event;
 
   if (httpMethod === 'OPTIONS') {
@@ -21,206 +33,338 @@ const handler = async (event, context) => {
       body: JSON.stringify({ success: true }),
     };
   }
+  // await connectDatabase();
+  // const { httpMethod, path } = event;
+  // const dishesId = path.split('/').pop();
 
+  // // Check authentication
+  // const authResult = authenticate(event);
+  // if (authResult.statusCode !== 200) {
+  //   return authResult; // Return early if authentication fails
+  // }
+
+  // // Proceed if authenticated
+  // const user = authResult.user;
+
+  // getting all dishes
   if (httpMethod === 'GET' && path.endsWith('/dishes')) {
     try {
-      const { dishes, dishesNumber } = await getDishes();
-
+      const dishes = await getDishes();
       return {
         statusCode: 200,
-        headers, // Include the headers in the response
-        body: JSON.stringify({
+        body: JSON.stringify(
           success: true,
           data: {
-            dishes: dishes,
-            dishesNumber: dishesNumber,
+            dishes,
+            countdishes,
           },
-        }),
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
       };
     } catch (error) {
-      console.error('Error:', error);
-      return {
-        statusCode: 500,
-        headers,
-        body: JSON.stringify({
-          success: false,
-          message: 'An internal server error occurred.',
-          error: error.message, // Include error details for debugging
-        }),
-      };
+      return handleError(error, 'fetching');
     }
   }
 
-  if (httpMethod === 'GET' && path.includes('/dishes/')) {
-    const dishId = path.split('/').pop(); // Extract dish id from path
-    try {
-      const dish = await getDish(dishId);
-      if (!dish) {
-        return {
-          statusCode: 404,
-          headers,
-          body: JSON.stringify({ message: 'dish not found' }),
-        };
-      }
-      return {
-        statusCode: 200,
-        headers, // Include the headers in the response
-        body: JSON.stringify({
-          success: true,
-          data: {
-            dish: dish,
-            dishId: dishId,
-          },
-        }),
-      };
-    } catch (error) {
-      console.error('Error:', error);
-      return {
-        statusCode: 500,
-        headers,
-        body: JSON.stringify({
-          success: false,
-          message: 'An internal server error occurred.',
-          error: error.message, // Include error details for debugging
-        }),
-      };
-    }
-  }
-
-  if (httpMethod === 'POST' && path.endsWith('/dishes/create')) {
-    try {
-      const dish = await createdish(body);
-      return {
-        statusCode: 200,
-        headers, // Include the headers in the response
-        body: JSON.stringify({
-          success: true,
-          data: dish.data,
-        }),
-      };
-    } catch (error) {
-      console.error('Error:', error);
-      return {
-        statusCode: 500,
-        headers,
-        body: JSON.stringify({
-          success: false,
-          message: 'An internal server error occurred.',
-          error: error.message, // Include error details for debugging
-        }),
-      };
-    }
-  }
+  //   // getting specific dish
+  //   if (httpMethod === 'GET' && path.endsWith(`/dishes/${dishesId}`)) {
+  //     try {
+  //       const dish = await Dishes.findById(dishesId);
+  //       if (!dish) {
+  //         return {
+  //           statusCode: 404,
+  //           body: JSON.stringify({ error: 'Dish not found' }),
+  //           headers: {
+  //             'Content-Type': 'application/json',
+  //             'Access-Control-Allow-Origin': '*',
+  //           },
+  //         };
+  //       }
+  //       return {
+  //         statusCode: 200,
+  //         body: JSON.stringify(dish),
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           'Access-Control-Allow-Origin': '*',
+  //         },
+  //       };
+  //     } catch (error) {
+  //       return handleError(error, 'fetching');
+  //     }
+  //   }
 
   return {
     statusCode: 405,
-    headers, // Include the headers in the response
-    body: JSON.stringify({
-      success: false,
-      message: 'Method Not Allowed',
-    }),
+    body: JSON.stringify({ error: 'Method not allowed.' }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
   };
-};
+}
 
-// GET request handler for all dishes
 async function getDishes() {
   try {
     const db = await getDb(); // Get the database connection
 
-    // Fetch all dishes using Mongoose
-    const dishes = await Dish.find({});
-    const dishesNumber = await Dish.countDocuments(); // Get total dishes
+    const allergies = await Allergen.find({}).exec();
+    const limit = 10;
+    //const allergenNames = allergies.map((allergen) => allergen.allergenName);
+    //const allergenIds = allergies.map((allergen) => allergen._id);
+    const allergens = allergies.map((allergen) => allergen.allergenName);
 
+    const ingrediends = await Dish.find({ ingrediends }).toArray();
+
+    // // Fetch all dishes using Mongoose
+    // const dishesNoAllergens = await Dish.find({
+    //   allergens: { $nin: allergens }, // Exclude allergens
+    // }).limit(limit); // limit
+
+    // const dishes = dishesNoAllergens.map((dish, index) => {
+    //   return {
+    //     ...dish.toObject(),
+    //   };
+    // });
+
+    // const countdishes = await Dish.countDocuments(); // Get total dishes
+    console.log(ingrediends, allergens);
     return {
       dishes: dishes,
-      dishesNumber: dishesNumber,
+      // countdishes: countdishes,
     };
   } catch (error) {
     console.error('Error fetching dishes:', error);
-    throw new Error('Failed to fetch dishes');
+    console.log(dishes);
+    throw new Error('Error fetching dishes');
   }
 }
 
-// GET request handler for dish with id
-async function getDish(dishId) {
-  try {
-    const db = await getDb();
-    if (!dishId || !ObjectId.isValid(dishId)) {
-      // check if valid mongodb id
-      // https://www.geeksforgeeks.org/how-to-check-if-a-string-is-valid-mongodb-objectid-in-node-js/
-      throw new Error('dish id not valid');
-    }
+// const mongoose = require('mongoose');
+// const getDb = require('./db_config/database.config.js'); // import getDatabase database from connection
+// const Allergen = require('./models/allergen.model'); // Import the Allergen model
+// const Dish = require('./models/dish.model'); // Import dish model
+// const { ObjectId } = require('mongodb'); // import ObjectId method to convert the _id field value to string [ https://www.mongodb.com/docs/manual/reference/method/ObjectId/ ]
 
-    // Find dish by _id using with findById() method
-    // const mongoosedishId = new mongoose.Types.ObjectId(dishId); // create the ObjectId
-    // const dish = await dish.findById(mongoosedishId);
-    const dish = await Dish.findById(dishId);
-    // console.log('dishId:', dishId);
-    // console.log('dish:', dish);
-    return dish || null;
-  } catch (error) {
-    console.error('Error fetching dish:', error);
-    throw new Error('Error fetching dish');
-  }
-}
+// const headers = {
+//   'Access-Control-Allow-Origin': '*',
+//   'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, OPTIONS',
+//   'Access-Control-Allow-Headers': 'Content-Type',
+// };
 
-// create class dish
-class dishObject {
-  constructor(reqBody) {
-    this.category = reqBody.category;
-    this.dishName = reqBody.dishName;
-    this.ingrediends = reqBody.ingrediends;
-    this.allergens = reqBody.allergens;
-    this.calories = reqBody.calories;
-    this.imageUrl = reqBody.imageUrl;
-  }
-}
+// const handler = async (event, context) => {
+//   //async function handler(event, context) {
+//   const { httpMethod, path, queryStringParameters, body } = event;
 
-// POST request handler to create dish
-async function createDish(body) {
-  try {
-    const db = await getDb();
+//   if (httpMethod === 'OPTIONS') {
+//     return {
+//       statusCode: 200,
+//       headers, // Return CORS headers for preflight
+//       body: JSON.stringify({ success: true }),
+//     };
+//   }
 
-    // Parse the body
-    const reqBody = JSON.parse(body); // Parse JSON string into an object
+//   if (httpMethod === 'GET' && path.endsWith('/dishes')) {
+//     try {
+//       const { dishes, dishesNumber } = await getDishes();
 
-    // Fetch all allergens [ https://www.mongodb.com/docs/manual/reference/operator/query/in/ ]
-    // $in  → Finds all documents where allergenName is in the provided array.
-    const allergens = await Allergen.find({
-      allergenName: {
-        $in: reqBody.allergies.map((allergen) => allergen.toLowerCase()),
-      },
-    });
+//       return {
+//         statusCode: 200,
+//         headers, // Include the headers in the response
+//         body: JSON.stringify({
+//           success: true,
+//           data: {
+//             dishes: dishes,
+//             dishesNumber: dishesNumber,
+//           },
+//         }),
+//       };
+//     } catch (error) {
+//       console.error('Error:', error);
+//       return {
+//         statusCode: 500,
+//         headers,
+//         body: JSON.stringify({
+//           success: false,
+//           message: 'An internal server error occurred.',
+//           error: error.message, // Include error details for debugging
+//         }),
+//       };
+//     }
+//   }
 
-    // allergens ids
-    const allergenIds = allergens.map((allergen) => allergen._id);
+//   if (httpMethod === 'GET' && path.includes('/dishes/')) {
+//     const dishId = path.split('/').pop(); // Extract dish id from path
+//     try {
+//       const dish = await getDish(dishId);
+//       if (!dish) {
+//         return {
+//           statusCode: 404,
+//           headers,
+//           body: JSON.stringify({ message: 'dish not found' }),
+//         };
+//       }
+//       return {
+//         statusCode: 200,
+//         headers, // Include the headers in the response
+//         body: JSON.stringify({
+//           success: true,
+//           data: {
+//             dish: dish,
+//             dishId: dishId,
+//           },
+//         }),
+//       };
+//     } catch (error) {
+//       console.error('Error:', error);
+//       return {
+//         statusCode: 500,
+//         headers,
+//         body: JSON.stringify({
+//           success: false,
+//           message: 'An internal server error occurred.',
+//           error: error.message, // Include error details for debugging
+//         }),
+//       };
+//     }
+//   }
 
-    // Create the dish object with the allergens and dietary restrictions
-    const dishData = new dishObject(reqBody, allergenIds);
-    // const dishData = {
-    //   dishName: reqBody.dishName,
-    //   allergies: allergens, // Include the ObjectIds of the allergens
-    //   dietaryRestrictions: reqBody.dietaryRestrictions, // Dietary restrictions as they are
-    // };
+//   if (httpMethod === 'POST' && path.endsWith('/dishes/create')) {
+//     try {
+//       const dish = await createdish(body);
+//       return {
+//         statusCode: 200,
+//         headers, // Include the headers in the response
+//         body: JSON.stringify({
+//           success: true,
+//           data: dish.data,
+//         }),
+//       };
+//     } catch (error) {
+//       console.error('Error:', error);
+//       return {
+//         statusCode: 500,
+//         headers,
+//         body: JSON.stringify({
+//           success: false,
+//           message: 'An internal server error occurred.',
+//           error: error.message, // Include error details for debugging
+//         }),
+//       };
+//     }
+//   }
 
-    const newdish = new dish(dishData); // create an new dish from model
+//   return {
+//     statusCode: 405,
+//     headers, // Include the headers in the response
+//     body: JSON.stringify({
+//       success: false,
+//       message: 'Method Not Allowed',
+//     }),
+//   };
+// };
 
-    const saveddish = await newdish.save(); // save to database
+// // GET request handler for all dishes
+// async function getDishes() {
+//   try {
+//     const db = await getDb(); // Get the database connection
 
-    // Populate the allergies field to get the full allergen documents
-    const dish = await dish
-      .findById(saveddish._id)
-      .populate('allergies') // replace ObjectIds with Allergen
-      .exec();
+//     // Fetch all dishes using Mongoose
+//     const dishes = await Dish.find({});
+//     const dishesNumber = await Dish.countDocuments(); // Get total dishes
 
-    // Return the new dish
-    return dish;
-  } catch (error) {
-    console.error('Error adding new dish:', error);
+//     return {
+//       dishes: dishes,
+//       dishesNumber: dishesNumber,
+//     };
+//   } catch (error) {
+//     console.error('Error fetching dishes:', error);
+//     throw new Error('Failed to fetch dishes');
+//   }
+// }
 
-    // Return error response
-    throw new Error('Error adding new dish');
-  }
-}
+// // GET request handler for dish with id
+// async function getDish(dishId) {
+//   try {
+//     const db = await getDb();
+//     if (!dishId || !ObjectId.isValid(dishId)) {
+//       // check if valid mongodb id
+//       // https://www.geeksforgeeks.org/how-to-check-if-a-string-is-valid-mongodb-objectid-in-node-js/
+//       throw new Error('dish id not valid');
+//     }
 
-module.exports.handler = handler;
+//     // Find dish by _id using with findById() method
+//     // const mongoosedishId = new mongoose.Types.ObjectId(dishId); // create the ObjectId
+//     // const dish = await dish.findById(mongoosedishId);
+//     const dish = await Dish.findById(dishId);
+//     // console.log('dishId:', dishId);
+//     // console.log('dish:', dish);
+//     return dish || null;
+//   } catch (error) {
+//     console.error('Error fetching dish:', error);
+//     throw new Error('Error fetching dish');
+//   }
+// }
+
+// // create class dish
+// class dishObject {
+//   constructor(reqBody) {
+//     this.category = reqBody.category;
+//     this.dishName = reqBody.dishName;
+//     this.ingrediends = reqBody.ingrediends;
+//     this.allergens = reqBody.allergens;
+//     this.calories = reqBody.calories;
+//     this.imageUrl = reqBody.imageUrl;
+//   }
+// }
+
+// // POST request handler to create dish
+// async function createDish(body) {
+//   try {
+//     const db = await getDb();
+
+//     // Parse the body
+//     const reqBody = JSON.parse(body); // Parse JSON string into an object
+
+//     // Fetch all allergens [ https://www.mongodb.com/docs/manual/reference/operator/query/in/ ]
+//     // $in  → Finds all documents where allergenName is in the provided array.
+//     const allergens = await Allergen.find({
+//       allergenName: {
+//         $in: reqBody.allergies.map((allergen) => allergen.toLowerCase()),
+//       },
+//     });
+
+//     // allergens ids
+//     const allergenIds = allergens.map((allergen) => allergen._id);
+
+//     // Create the dish object with the allergens and dietary restrictions
+//     const dishData = new dishObject(reqBody, allergenIds);
+//     // const dishData = {
+//     //   dishName: reqBody.dishName,
+//     //   allergies: allergens, // Include the ObjectIds of the allergens
+//     //   dietaryRestrictions: reqBody.dietaryRestrictions, // Dietary restrictions as they are
+//     // };
+
+//     const newdish = new dish(dishData); // create an new dish from model
+
+//     const saveddish = await newdish.save(); // save to database
+
+//     // Populate the allergies field to get the full allergen documents
+//     const dish = await dish
+//       .findById(saveddish._id)
+//       .populate('allergies') // replace ObjectIds with Allergen
+//       .exec();
+
+//     // Return the new dish
+//     return dish;
+//   } catch (error) {
+//     console.error('Error adding new dish:', error);
+
+//     // Return error response
+//     throw new Error('Error adding new dish');
+//   }
+// }
+
+// module.exports.handler = handler;
