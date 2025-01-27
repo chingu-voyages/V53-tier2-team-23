293ce9338cf3cfd0225cbf034edeae7efce3d028
leonalkalai <koundouras@gmail.com>
2025-01-27 19:42:28 +0200

    fix

--- api/src/dishes.js
+++ api/src/dishes.js
@@ -39,7 +39,7 @@ async function handler(event, context) {
         body: JSON.stringify({
           success: true,
           data: {
-            dishes
+            dishes,
             allergens,
             unsafeIngredients,
           },
