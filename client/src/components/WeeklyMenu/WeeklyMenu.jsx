2daef70e7e5646f1749c5182d644a073437145dc
Thea Win <mstheawin@gmail.com>
2025-02-03 21:28:44 +0800

    combined with DayNavigator and implemented ExportToPDF button
    DayNavigator component no longer needed for another page, so, putting everything in this page.
    ExportToPDF is created in another file as it might look neater this way

--- client/src/components/WeeklyMenu/WeeklyMenu.jsx
+++ client/src/components/WeeklyMenu/WeeklyMenu.jsx
@@ -1,12 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { FaArrowDown } from 'react-icons/fa6';
-import DayNavigator from '../DayNavigator/DayNavigator';
+import { LuCircleArrowRight, LuCircleArrowLeft } from 'react-icons/lu';
+import ExportToPDF from '../ExportToPDF/ExportToPDF';
 
 function WeeklyMenu() {
   const [selectedDate, setSelectedDate] = useState(null);
   const [menu, setMenu] = useState(null);
   const [dish, setDish] = useState(null);
   const [weekDates, setWeekDates] = useState([]);
+
+  // to scroll horizontally when the user interacts withthe scroll wheel for small screen size
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    const scrollContainer = scrollRef.current;
+    if (!scrollContainer) return;
+
+    const handleWheelScroll = (event) => {
+      event.preventDefault();
+      scrollContainer.scrollLeft += event.deltaY;
+    };
+
+    scrollContainer.addEventListener('wheel', handleWheelScroll);
+    return () =>
+      scrollContainer.removeEventListener('wheel', handleWheelScroll);
+  }, []);
 
   // to fetch weekly menu
   const fetchWeeklyMenu = async (weekStart) => {
@@ -57,11 +75,64 @@ function WeeklyMenu() {
 
   return (
     <div>
-      <DayNavigator
-        selectedDate={selectedDate}
-        setSelectedDate={setSelectedDate}
-        weekDates={weekDates}
-      />
+      <div className='flex flex-col items-center border-t-2 border-b-2 lg:border-2 border-primary rounded-lg shadow-md pb-2 md:pb-[0.15rem] lg:mt-[19px] lg:w-[845px] mx-auto '>
+        <div className='flex items-center justify-between w-full px-[1.35rem] md:px-[4rem] lg:px-[4.2rem]  '>
+          <button className='text-gray-400 relative'>
+            <LuCircleArrowLeft className='w-11 h-10 md:w-13 md:h-12' />
+          </button>
+          <div className='text-center pt-1 md:pt-[0.4rem]'>
+            <div className='text-[16px]'>Week of</div>
+            <div className='text-[16px] font-bold'>
+              {weekDates.length > 0 &&
+                new Date(weekDates[0].fullDate).toLocaleDateString('en-US', {
+                  month: 'long',
+                  day: 'numeric',
+                })}
+            </div>
+          </div>
+          <button className='text-primary relative'>
+            <LuCircleArrowRight className='w-11 h-10 md:w-13 md:h-12' />
+          </button>
+        </div>
+        <div
+          ref={scrollRef}
+          className='overflow-x-auto snap-x scrollbar-hide md:overflow-visible md:snap-none flex w-full mt-1 gap-[19.5px] md:gap-5 ml-4 md:ml-0 md:justify-center md:mt-4'
+        >
+          {weekDates.map((item) => {
+            const isDisabled = item.dish === null;
+            const isSelected = selectedDate === item.fullDate;
+
+            return (
+              <div
+                key={item.fullDate}
+                onClick={() => !isDisabled && setSelectedDate(item.fullDate)}
+                className={`cursor-pointer snap-start flex flex-col flex-[0_0_auto] items-center justify-center w-[70px] h-[72px] md:h-[70px] pt-[3px] border border-black rounded-md ${
+                  isDisabled
+                    ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
+                    : isSelected
+                    ? 'text-white bg-primary'
+                    : 'bg-[#F0EBF6]'
+                }`}
+              >
+                <div
+                  className={`text-[11px] ${
+                    isSelected ? 'font-normal' : 'font-bold'
+                  }`}
+                >
+                  {item.day}
+                </div>
+                <div
+                  className={`text-[11px] ${
+                    isSelected ? 'font-normal' : 'font-bold'
+                  }`}
+                >
+                  {item.date}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <h1 className='text-center text-primary font-bold text-[24px] mt-[1.35rem] font-shantell'>
         Check this Week&apos;s Meals
       </h1>
@@ -93,9 +164,12 @@ function WeeklyMenu() {
             </div>
           </div>
           <div className='mt-6 flex flex-col gap-7 items-center'>
-            <button className='bg-primary text-white p-1 px-6 rounded-full text-[24px] flex items-center justify-center gap-2 shadow-lg w-fit'>
+            <button
+              className='bg-primary text-white p-1 px-6 rounded-full text-[24px] flex items-center justify-center gap-2 shadow-lg w-fit'
+            >
               EXPORT WEEKLY PLAN <FaArrowDown />
             </button>
+            <ExportToPDF weekDates={weekDates} />
             <button className='border-[1px] border-primary text-primary py-1 px-4 rounded-full font-semibold text-[24px] font-shantell shadow-lg w-fit'>
               EDIT MEAL
             </button>
