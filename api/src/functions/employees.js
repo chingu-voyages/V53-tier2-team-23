9690608347c7daf05c851e5af6d77a971c8518fd
leonalkalai <koundouras@gmail.com>
2025-02-03 19:01:55 +0200

    local

--- api/src/functions/employees.js
+++ api/src/functions/employees.js
@@ -112,9 +112,9 @@ exports.handler = async (event) => {
     return {
       statusCode: 204,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'http://localhost:5173',
         'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },
     };
   }
