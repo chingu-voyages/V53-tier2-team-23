12dd9ee690eb6e6c7af93b8584420864c6040c97
leonalkalai <koundouras@gmail.com>
2025-02-03 18:35:14 +0200

    created employee create form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import CreateEmployee from './components/CreateEmployee/CreateEmployee';
 
 function App() {
   return (
     <>
       <NavBar />
+      <CreateEmployee />
       <main className='flex-grow bg-gray-100'>
         <div className='h-[872px] md:h-[990px] lg:h-[907px] bg-gray-200 rounded-md flex items-center justify-center'>
           <p className='text-lg text-gray-600'>Placeholder Content</p>
