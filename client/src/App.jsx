fce77375828bd61498131373d142c23cbb790997
leonalkalai <koundouras@gmail.com>
2025-02-04 13:43:12 +0200

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import LoginPage from './components/LoginPage/LoginPage';
-import DatePickerContainer from './components/DatePicker/DatePickerContainer';
 import './index.css';
 
 function App() {
@@ -9,9 +8,6 @@ function App() {
     <>
       <NavBar />
       <LoginPage />
-      {/* <div className='custom-bg flex justify-between'>
-        <DatePickerContainer />
-      </div> */}
       <main className='flex-grow bg-gray-100'>
         <div className='h-[872px] md:h-[990px] lg:h-[907px] bg-gray-200 rounded-md flex items-center justify-center'>
           <p className='text-lg text-gray-600'>Placeholder Content</p>
