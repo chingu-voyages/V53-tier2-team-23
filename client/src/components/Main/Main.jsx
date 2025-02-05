eab71eb31fd15e7aa1eb20f87caf846e26fac5d8
leonalkalai <koundouras@gmail.com>
2025-02-05 15:18:21 +0200

    cs final

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -53,15 +53,19 @@ function Main() {
     <main
       className={`${customBg} w-full mt-[20px] flex flex-grow flex-col bg-gray-100`}
     >
-      <span
-        className={`${welcome}  mt-[20px] text-center text-lg text-[#492470] font-bold`}
-      >
-        Welcome back! 👋
-      </span>
-      <h2 className={`${formContainerTitle} text-[#492470] text-4xl font-bold`}>
-        Weekly menu Home page
-      </h2>
-      <div className='buttons-container flex flex-col flex-wrap gap-10 bg-white min-w-[250px] p-10 my-[5%] mx-auto h-auto box-border w-1/2 min-h-[30vh] items-center justify-center'>
+      <div className='buttons-container shadow-md px-4 py-4 rounded-[10px] flex flex-col flex-wrap gap-5 bg-white min-w-[250px] p-10 my-[5%] mx-auto h-auto box-border w-1/2 min-h-[510px] max-w-[430px] items-center justify-start'>
+        <div className='flex flex-col items-center mb-20'>
+          <span
+            className={`${welcome}  m-[20px] text-center text-lg text-[#492470] font-bold`}
+          >
+            Welcome back! 👋
+          </span>
+          <h2
+            className={`${formContainerTitle} text-[#492470] xs:text-2xl text-xl font-bold`}
+          >
+            Weekly menu Home page
+          </h2>
+        </div>
         <button
           onClick={handleEnterEmployeeView}
           type='button'
