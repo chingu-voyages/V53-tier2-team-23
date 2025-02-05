78203d0ccd4e37d74f5054125855f21950ba4397
Thea Win <mstheawin@gmail.com>
2025-02-05 19:45:34 +0400

    select employee feature

--- client/src/components/Management/Management.jsx
+++ client/src/components/Management/Management.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 function Management() {
   const [buttonText, setButtonText] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [employees, setEmployees] = useState([]);
   const navigate = useNavigate();
   const location = useLocation();
   const { username } = location.state?.username || {};
@@ -12,6 +14,37 @@ function Management() {
     setButtonText(action);
     navigate('/calendar', { state: { action, isViewMode: action === 'View' } });
   };
+
+  const token = localStorage.getItem('token');
+
+  // to fetch employee list
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await fetch(
+          'https://eato-meatplanner.netlify.app/.netlify/functions/employees',
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const responseData = await response.json();
+        console.log('employee data: ', responseData);
+
+        if (responseData.data) {
+          setEmployees(responseData.data);
+        } else {
+          console.error('Unexpected API response format:', responseData);
+        }
+      } catch (error) {
+        console.error('Error fetching employees: ', error);
+      }
+    };
+    fetchEmployees();
+  }, []);
 
   return (
     <div>
@@ -43,7 +76,36 @@ function Management() {
             Allergies
           </p>
           <div className='hidden group-hover:flex flex-col gap-3 mt-2 w-fit'>
-            <button>Select an Employee</button>
+            <div className='relative'>
+              <button
+                onClick={() => setShowDropdown((prev) => !prev)}
+                className='text-primary px-4 py-2'
+              >
+                Select an Employee
+              </button>
+              {/* dropdown list of employees */}
+              {showDropdown && (
+                <div>
+                  {employees.length > 0 ? (
+                    employees.map((employee) => (
+                      <button
+                        key={employee._id}
+                        className='block w-full text-left px-4 py-2 hover:bg-gray-100'
+                        onClick={() =>
+                          navigate('/edit-employee', {
+                            state: { employeeId: employee._id },
+                          })
+                        }
+                      >
+                        {employee.employeeName}
+                      </button>
+                    ))
+                  ) : (
+                    <p>No employees available</p>
+                  )}
+                </div>
+              )}
+            </div>
             <button onClick={() => navigate('/create-employee')}>
               Add an Employee
             </button>
