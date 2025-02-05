187b8c6c2feaa1daf70ff99b35a18e2ddbe840a0
leonalkalai <koundouras@gmail.com>
2025-02-05 14:20:42 +0200

    created css for main

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './../LoginPage/LoginPage.module.css';
+import Mainstyles from './Main.module.css';
 import '../../index.css';
 
 function Main() {
@@ -10,7 +11,6 @@ function Main() {
     formContainerTitle: styles['form-container__title'],
     formContainerForm: styles['form-container__form'],
     formContainerInput: styles['form-container__input'],
-    formContainerButton: styles['form-container__button'],
     formContainerResponse: styles['form-container__response'],
     welcome: styles['welcome'],
   };
@@ -21,10 +21,21 @@ function Main() {
     formContainerTitle,
     formContainerForm,
     formContainerInput,
-    formContainerButton,
     formContainerResponse,
     welcome,
   } = customStyles;
+
+  const bgStyles = {
+    customBg: Mainstyles['custom-bg'],
+  };
+
+  const buttonStyles = {
+    MainButton: styles['main-button'],
+  };
+
+  const { customBg } = bgStyles;
+
+  const { MainButton } = buttonStyles;
 
   const navigate = useNavigate();
 
@@ -33,29 +44,62 @@ function Main() {
     navigate('/login');
   };
 
+  const handleEnterEmployeeView = (event) => {
+    event.preventDefault();
+    navigate('/menu');
+  };
+
   return (
-    <main className='flex flex-grow flex-col bg-gray-100'>
-      <button
-        onClick={handleEnterManagerDashboard}
-        id='loginButton'
-        type='button'
-        className={`${formContainerButton}
-          rounded-md
-          hover:border-yellow-300
-          p-[10px_20px]
-          box-border
-          border-yellow-400
-          bg-white
-          text-black
-          border-4
-          border-solid
-          uppercase
-          `}
+    <main
+      className={`${customBg} w-full mt-[20px] flex flex-grow flex-col bg-gray-100`}
+    >
+      <span
+        className={`${welcome}  mt-[20px] text-center text-lg text-[#492470] font-bold`}
       >
-        Go to Manager Dashboard
-      </button>
-      <div className='h-[872px] md:h-[990px] lg:h-[907px] bg-gray-200 rounded-md flex items-center justify-center'>
-        <p className='text-lg text-gray-600'>Placeholder Content</p>
+        Welcome back! 👋
+      </span>
+      <h2 className={`${formContainerTitle} text-[#492470] text-4xl font-bold`}>
+        Weekly menu Home page
+      </h2>
+      <div className='buttons-container flex flex-col flex-wrap gap-10 bg-white min-w-[250px] p-10 my-[5%] mx-auto h-auto box-border w-1/2 min-h-[30vh] items-center justify-center'>
+        <button
+          onClick={handleEnterEmployeeView}
+          type='button'
+          className={`${MainButton}
+          rounded-md
+          bg-[#492470] 
+          hover:bg-[#67369c] 
+          p-[10px_50px]
+          box-border
+          border-none
+          text-white
+          uppercase
+          xs:text-xl
+          text-sm
+          `}
+        >
+          Enter as an Employee
+        </button>
+        <button
+          onClick={handleEnterManagerDashboard}
+          type='button'
+          className={`${MainButton}
+          rounded-[6px]
+          bg-white
+          hover:bg-slate-200     
+          p-[10px_50px]
+          border-[#492470]
+          box-border
+          border-2
+          border-solid
+          text-[#492470]
+          uppercase
+          xs:text-xl
+          text-sm
+          `}
+        >
+          Enter as a Manager
+        </button>
       </div>
     </main>
   );
