1e8b2429643536bffddbd53b6e3c3eee774e5645
Thea Win <mstheawin@gmail.com>
2025-02-15 13:46:09 +0400

    add button click function for "Menu"

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { CgProfile } from 'react-icons/cg'; // placeholder for profile icon
 import Logo from '../../assets/logo5.svg';
+import MenuSection from '../MenuSection/MenuSection';
+import { useNavigate } from 'react-router-dom';
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const handleMenuButtonClick = (action) => {
+    navigate('/calendar', { state: { action, isViewMode: action === 'View' } });
+  };
+
   return (
     <nav className='flex gap-16 justify-between p-[0.55rem] pt-3.5 pb-[1.7rem] md:px-8 md:pt-7 md:pb-[1.6rem] lg:pl-[5.5rem] lg:pt-[0.5rem] lg:pb-[2.1rem]'>
       <div className='flex '>
@@ -17,7 +25,17 @@ function NavBar() {
       </div>
       <div className='items-center gap-8 hidden sm:flex text-primary'>
         <ul className='flex gap-[1rem] text-[24px] font-medium px-[0.4rem] pt-2 lg:font-semibold lg:gap-[4.7rem] lg:pt-[2rem] lg:pr-10'>
-          <li>Menus</li>
+          <div className='group relative'>
+            <li className='relative'>
+              <p className='cursor-pointer'>Menu</p>
+              <div className='absolute right-0 top-full z-20'>
+                <MenuSection
+                  onGenerate={() => handleMenuButtonClick('Generate')}
+                  onView={() => handleMenuButtonClick('View')}
+                />
+              </div>
+            </li>
+          </div>
           {/*  will need to later hide Allergies if no user log in */}
           <li>Allergies</li>
         </ul>
