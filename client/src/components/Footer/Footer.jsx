3224078145d9b5f12a679eedfcc275e6b0696bdd
Thea Win <mstheawin@gmail.com>
2025-02-15 16:58:21 +0400

    header and footer update
    all buttons working

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,30 +1,34 @@
 import HomeIcon from '../../assets/Home.svg';
 import MenuIcon from '../../assets/Menu.svg';
 import { CgProfile } from 'react-icons/cg';
+import LogOut from '../LogOut.jsx/LogOut';
 import MenuSection from '../MenuSection/MenuSection';
 import EmployeeSection from '../EmployeeSection/EmployeeSection';
-import LogOut from '../LogOut.jsx/LogOut';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Footer() {
   const navigate = useNavigate();
+  const [showNavOptions, setShowNavOptions] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    setIsLoggedIn(!!token);
+  }, [token]);
 
   const handleLogoClick = () => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      navigate('/management');
-    } else {
-      navigate('/');
-    }
+    navigate(token ? '/management' : '/');
   };
 
   const handleMenuButtonClick = (action) => {
+    setShowNavOptions(false);
     navigate('/calendar', { state: { action, isViewMode: action === 'View' } });
   };
 
   const handleEmployeeSelect = (employee) => {
     sessionStorage.setItem('clearSession', 'true');
+    setShowNavOptions(false);
     navigate('/manage-allergies', {
       state: {
         employeeData: {
@@ -39,8 +43,13 @@ function Footer() {
     navigate('/create-employee');
   };
 
+  const toggleNavOptions = () => {
+    setShowNavOptions((prev) => !prev);
+  };
+
   return (
-    <footer className='flex items-center justify-between px-6 py-5 md:py-[0.9rem] md:px-9 lg:px-[2.35rem] lg:py-4'>
+    <footer className='relative flex items-center justify-between px-6 py-5 md:py-[0.9rem] md:px-9 lg:px-[2.35rem] lg:py-4 bg-white border-t border-primary'>
+      {/* Footer Content */}
       <div className='flex space-x-14 pl-1 pb-2 md:pl-[0.7rem] md:pt-[0.2rem] md:space-x-16 lg:pt-[0.55rem] lg:space-x-10'>
         <button onClick={handleLogoClick}>
           <img
@@ -53,21 +62,63 @@ function Footer() {
         <a
           href='https://github.com/chingu-voyages/V53-tier2-team-23/tree/main'
           target='_blank'
+          rel='noopener noreferrer'
           className='text-primary text-[20px] font-medium relative pt-2 md:pt-[0.85rem] md:pl-[0.85rem] lg:pl-[2.8rem]'
         >
           View Our GitHub
         </a>
       </div>
 
-      <div className='hidden sm:flex group'>
-        <CgProfile className='md:w-14 md:h-14 lg:w-16 lg:h-16 text-primary' />
-        <div className='absolute right-0 top-auto z-20'>
-          <LogOut />
+      {isLoggedIn && (
+        <div>
+          <div className='hidden sm:flex group'>
+            <CgProfile className='w-14 h-14 lg:w-16 lg:h-16 text-primary' />
+            <div className='absolute right-0 top-auto z-20'>
+              <LogOut />
+            </div>
+          </div>
+
+          <button
+            onClick={toggleNavOptions}
+            className='text-primary relative sm:hidden'
+          >
+            <img
+              src={MenuIcon}
+              className='w-10 h-10 md:w-12 md:h-12'
+              alt='Menu'
+            />
+          </button>
         </div>
-      </div>
-      <button className='sm:hidden '>
-        <img src={MenuIcon} alt='Menu Icon' className='w-15 h-15' />
-      </button>
+      )}
+
+      {/* Dropdown Menu */}
+      {showNavOptions && (
+        <div className='absolute bottom-full right-0 w-full bg-white shadow-lg border-t border-gray-300 transition-transform z-20'>
+          <div className='group relative'>
+            <p className='cursor-pointer block w-full p-4 text-primary font-medium text-2xl bg-white border-[1px] border-primary text-center'>
+              Menus
+            </p>
+            <div className='absolute left-1/2 bottom-full -translate-x-1/2 z-20'>
+              <MenuSection
+                onGenerate={() => handleMenuButtonClick('Generate')}
+                onView={() => handleMenuButtonClick('View')}
+              />
+            </div>
+          </div>
+
+          <div className='group relative'>
+            <p className='cursor-pointer block w-full p-4 text-primary font-medium text-2xl bg-white border-[1px] border-primary text-center'>
+              Allergies
+            </p>
+            <div className='absolute left-1/2 bottom-full -translate-x-1/2 z-20'>
+              <EmployeeSection
+                onSelectEmployee={handleEmployeeSelect}
+                onAddEmployee={handleAddEmployee}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </footer>
   );
 }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CgProfile } from 'react-icons/cg'; // placeholder for profile icon
 import Logo from '../../assets/logo5.svg';
 import MenuSection from '../MenuSection/MenuSection';
@@ -8,15 +8,15 @@ import { useNavigate } from 'react-router-dom';
 
 function NavBar() {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    setIsLoggedIn(!!token);
+  }, [token]);
 
   const handleLogoClick = () => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      navigate('/management');
-    } else {
-      navigate('/');
-    }
+    navigate(token ? '/management' : '/');
   };
 
   const handleMenuButtonClick = (action) => {
@@ -51,43 +51,50 @@ function NavBar() {
           MenuHelp
         </div>
       </div>
-      <div className='items-center gap-8 hidden sm:flex '>
-        <ul className='flex gap-[1rem] px-[0.4rem] pt-2  lg:gap-[4.7rem] lg:pt-[2rem] lg:pr-10'>
-          <div className='group relative'>
-            <li className='relative'>
-              <p className='cursor-pointer text-primary text-[24px] font-medium lg:font-semibold'>
-                Menu
-              </p>
-              <div className='absolute right-0 top-full z-20'>
-                <MenuSection
-                  onGenerate={() => handleMenuButtonClick('Generate')}
-                  onView={() => handleMenuButtonClick('View')}
-                />
+      {isLoggedIn && (
+        <div>
+          <div className='items-center gap-8 hidden sm:flex '>
+            <ul className='flex gap-[1rem] px-[0.4rem] pt-2  lg:gap-[4.7rem] lg:pt-[2rem] lg:pr-10'>
+              <div className='group relative'>
+                <li className='relative'>
+                  <p className='cursor-pointer text-primary text-[24px] font-medium lg:font-semibold'>
+                    Menu
+                  </p>
+                  <div className='absolute right-0 top-full z-20'>
+                    <MenuSection
+                      onGenerate={() => handleMenuButtonClick('Generate')}
+                      onView={() => handleMenuButtonClick('View')}
+                    />
+                  </div>
+                </li>
               </div>
-            </li>
-          </div>
-          {/*  will need to later hide Allergies if no user log in */}
-          <div className='group relative'>
-            <li className='relative'>
-              <p className='cursor-pointer text-primary text-[24px] font-medium lg:font-semibold'>
-                Allergies
-              </p>
-              <div className='absolute right-0 top-full z-20'>
-                <EmployeeSection
-                  onSelectEmployee={handleEmployeeSelect}
-                  onAddEmployee={handleAddEmployee}
-                />
+              {/*  will need to later hide Allergies if no user log in */}
+              <div className='group relative'>
+                <li className='relative'>
+                  <p className='cursor-pointer text-primary text-[24px] font-medium lg:font-semibold'>
+                    Allergies
+                  </p>
+                  <div className='absolute right-0 top-full z-20'>
+                    <EmployeeSection
+                      onSelectEmployee={handleEmployeeSelect}
+                      onAddEmployee={handleAddEmployee}
+                    />
+                  </div>
+                </li>
               </div>
-            </li>
+            </ul>
           </div>
-        </ul>
-      </div>
-      <div className='group relative sm:hidden'>
-        <CgProfile size={40} className='absolute top-4 right-7 text-primary' />
-        <div className='absolute right-0 top-full z-20'>
-          <LogOut />
+          <div className='group relative sm:hidden'>
+            <CgProfile
+              size={40}
+              className='absolute top-4 right-7 text-primary'
+            />
+            <div className='absolute right-0 top-full z-20'>
+              <LogOut />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </nav>
   );
 }
