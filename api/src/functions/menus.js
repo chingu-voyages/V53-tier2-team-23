8e09202f614781a310c74e3b91d53046a47e1ed0
leonalkalai <koundouras@gmail.com>
2025-02-16 11:29:33 +0200

    test menu

--- api/src/functions/menus.js
+++ api/src/functions/menus.js
@@ -73,7 +73,8 @@ exports.handler = async (event) => {
       if (!weekStartDate || !Array.isArray(days) || days.length === 0) {
         return sendResponse(
           400,
-          'Week start date and day arrays are required.'
+          'Week start date and day arrays are required.',
+          origin
         );
       }
 
@@ -85,19 +86,24 @@ exports.handler = async (event) => {
       if (existingMenu) {
         return sendResponse(
           409,
-          `A menu already exists for the week starting on ${weekStartDate}.`
+          `A menu already exists for the week starting on ${weekStartDate}.`,
+          origin
         );
       }
 
       for (const day of days) {
         if (!day.date) {
-          return sendResponse(400, 'Each day must have a date');
+          return sendResponse(400, 'Each day must have a date', origin);
         }
 
         if (day.dish) {
           const dishExists = await Dishes.findById(day.dish);
           if (!dishExists) {
-            return sendResponse(400, `Dish with ID ${day.dish} does not exist`);
+            return sendResponse(
+              400,
+              `Dish with ID ${day.dish} does not exist`,
+              origin
+            );
           }
         }
       }
@@ -107,9 +113,9 @@ exports.handler = async (event) => {
         days,
       });
 
-      return sendResponse(200, 'Menu created successfully', newMenu);
+      return sendResponse(200, 'Menu created successfully', newMenu, origin);
     } catch (error) {
-      return handleError(error, 'creating');
+      return handleError(error, 'creating', origin);
     }
   }
 
@@ -120,7 +126,11 @@ exports.handler = async (event) => {
 
       // Validate input
       if (!weekStartDate || !date) {
-        return sendResponse(400, 'weekStartDate and date are required.');
+        return sendResponse(
+          400,
+          'weekStartDate and date are required.',
+          origin
+        );
       }
 
       const menu = await Menus.findOne({
@@ -128,7 +138,11 @@ exports.handler = async (event) => {
       });
 
       if (!menu) {
-        return sendResponse(404, 'Menu not found for the given weekStartDate');
+        return sendResponse(
+          404,
+          'Menu not found for the given weekStartDate',
+          origin
+        );
       }
 
       // Find the specific day to update
@@ -138,13 +152,17 @@ exports.handler = async (event) => {
       );
 
       if (!dayToUpdate) {
-        return sendResponse(404, `No day found for the date: ${date}`);
+        return sendResponse(404, `No day found for the date: ${date}`, origin);
       }
 
       if (dish) {
         const dishExists = await Dishes.findById(dish);
         if (!dishExists) {
-          return sendResponse(400, `Dish with ID ${dish} does not exist.`);
+          return sendResponse(
+            400,
+            `Dish with ID ${dish} does not exist.`,
+            origin
+          );
         }
 
         const isDishAlreadyAssigned = menu.days.some(
@@ -155,13 +173,18 @@ exports.handler = async (event) => {
           console.log(`Dish with ID ${dish} is already assigned in this week.`);
           return sendResponse(
             400,
-            `Duplicate dish found. Each dish can only be used once per week.`
+            `Duplicate dish found. Each dish can only be used once per week.`,
+            origin
           );
         }
       } else {
         const nullDishCount = menu.days.filter((day) => !day.dish).length;
         if (nullDishCount >= 2) {
-          return sendResponse(400, 'Only 2 days off are allowed per week.');
+          return sendResponse(
+            400,
+            'Only 2 days off are allowed per week.',
+            origin
+          );
         }
       }
 
@@ -172,9 +195,14 @@ exports.handler = async (event) => {
         weekStartDate: new Date(weekStartDate),
       }).populate('days.dish');
 
-      return sendResponse(200, 'Menu updated successfully', updatedMenu);
+      return sendResponse(
+        200,
+        'Menu updated successfully',
+        updatedMenu,
+        origin
+      );
     } catch (error) {
-      return handleError(error, 'updating specific day');
+      return handleError(error, 'updating specific day', origin);
     }
   }
 
@@ -184,7 +212,11 @@ exports.handler = async (event) => {
       const { weekStartDate } = queryStringParameters;
 
       if (!weekStartDate) {
-        return sendResponse(400, 'weekStartDate query parameter is required.');
+        return sendResponse(
+          400,
+          'weekStartDate query parameter is required.',
+          origin
+        );
       }
 
       const deletedMenu = await Menus.findOneAndDelete({
@@ -192,12 +224,16 @@ exports.handler = async (event) => {
       });
 
       if (!deletedMenu) {
-        return sendResponse(404, 'Menu not found for the given weekStartDate');
+        return sendResponse(
+          404,
+          'Menu not found for the given weekStartDate',
+          origin
+        );
       }
 
-      return sendResponse(200, 'Menu deleted successfully');
+      return sendResponse(200, 'Menu deleted successfully', origin);
     } catch (error) {
-      return handleError(error, 'deleting');
+      return handleError(error, 'deleting', origin);
     }
   }
 
@@ -207,7 +243,11 @@ exports.handler = async (event) => {
       const { weekStartDate } = queryStringParameters;
 
       if (!weekStartDate) {
-        return sendResponse(400, 'weekStartDate query parameter is required.');
+        return sendResponse(
+          400,
+          'weekStartDate query parameter is required.',
+          origin
+        );
       }
 
       const menu = await Menus.findOne({
@@ -215,14 +255,180 @@ exports.handler = async (event) => {
       }).populate('days.dish');
 
       if (!menu) {
-        return sendResponse(404, 'Menu not found for the given weekStartDate.');
+        return sendResponse(
+          404,
+          'Menu not found for the given weekStartDate.',
+          origin
+        );
       }
 
-      return sendResponse(200, 'Menu fetched successfully', menu);
+      return sendResponse(200, 'Menu fetched successfully', menu, origin);
     } catch (error) {
-      return handleError(error, 'fetching');
+      return handleError(error, 'fetching', origin);
     }
   }
 
   return sendResponse(405, 'Method not allowed.', origin);
+
+  // // Create new Menu (POST)
+  // if (httpMethod === 'POST' && path.endsWith('/menus')) {
+  //   try {
+  //     const { weekStartDate, days } = JSON.parse(body);
+
+  //     // Validate input
+  //     if (!weekStartDate || !Array.isArray(days) || days.length === 0) {
+  //       return sendResponse(
+  //         400,
+  //         'Week start date and day arrays are required.'
+  //       );
+  //     }
+
+  //     // to check if a menu already exists
+  //     const existingMenu = await Menus.findOne({
+  //       weekStartDate: new Date(weekStartDate),
+  //     });
+
+  //     if (existingMenu) {
+  //       return sendResponse(
+  //         409,
+  //         `A menu already exists for the week starting on ${weekStartDate}.`
+  //       );
+  //     }
+
+  //     for (const day of days) {
+  //       if (!day.date) {
+  //         return sendResponse(400, 'Each day must have a date');
+  //       }
+
+  //       if (day.dish) {
+  //         const dishExists = await Dishes.findById(day.dish);
+  //         if (!dishExists) {
+  //           return sendResponse(400, `Dish with ID ${day.dish} does not exist`);
+  //         }
+  //       }
+  //     }
+
+  //     const newMenu = await Menus.create({
+  //       weekStartDate,
+  //       days,
+  //     });
+
+  //     return sendResponse(200, 'Menu created successfully', newMenu);
+  //   } catch (error) {
+  //     return handleError(error, 'creating');
+  //   }
+  // }
+
+  // // Edit menu (PUT)
+  // if (httpMethod === 'PUT' && path.endsWith('/menus')) {
+  //   try {
+  //     const { weekStartDate, date, dish } = JSON.parse(body);
+
+  //     // Validate input
+  //     if (!weekStartDate || !date) {
+  //       return sendResponse(400, 'weekStartDate and date are required.');
+  //     }
+
+  //     const menu = await Menus.findOne({
+  //       weekStartDate: new Date(weekStartDate),
+  //     });
+
+  //     if (!menu) {
+  //       return sendResponse(404, 'Menu not found for the given weekStartDate');
+  //     }
+
+  //     // Find the specific day to update
+  //     const dayToUpdate = menu.days.find(
+  //       (day) =>
+  //         new Date(day.date).toISOString() === new Date(date).toISOString()
+  //     );
+
+  //     if (!dayToUpdate) {
+  //       return sendResponse(404, `No day found for the date: ${date}`);
+  //     }
+
+  //     if (dish) {
+  //       const dishExists = await Dishes.findById(dish);
+  //       if (!dishExists) {
+  //         return sendResponse(400, `Dish with ID ${dish} does not exist.`);
+  //       }
+
+  //       const isDishAlreadyAssigned = menu.days.some(
+  //         (day) => day.dish && day.dish.toString() === dish
+  //       );
+
+  //       if (isDishAlreadyAssigned) {
+  //         console.log(`Dish with ID ${dish} is already assigned in this week.`);
+  //         return sendResponse(
+  //           400,
+  //           `Duplicate dish found. Each dish can only be used once per week.`
+  //         );
+  //       }
+  //     } else {
+  //       const nullDishCount = menu.days.filter((day) => !day.dish).length;
+  //       if (nullDishCount >= 2) {
+  //         return sendResponse(400, 'Only 2 days off are allowed per week.');
+  //       }
+  //     }
+
+  //     dayToUpdate.dish = dish || null;
+
+  //     await menu.save();
+  //     const updatedMenu = await Menus.findOne({
+  //       weekStartDate: new Date(weekStartDate),
+  //     }).populate('days.dish');
+
+  //     return sendResponse(200, 'Menu updated successfully', updatedMenu);
+  //   } catch (error) {
+  //     return handleError(error, 'updating specific day');
+  //   }
+  // }
+
+  // // Delete menu (DELETE)
+  // if (httpMethod === 'DELETE' && path.endsWith('/menus')) {
+  //   try {
+  //     const { weekStartDate } = queryStringParameters;
+
+  //     if (!weekStartDate) {
+  //       return sendResponse(400, 'weekStartDate query parameter is required.');
+  //     }
+
+  //     const deletedMenu = await Menus.findOneAndDelete({
+  //       weekStartDate: new Date(weekStartDate),
+  //     });
+
+  //     if (!deletedMenu) {
+  //       return sendResponse(404, 'Menu not found for the given weekStartDate');
+  //     }
+
+  //     return sendResponse(200, 'Menu deleted successfully');
+  //   } catch (error) {
+  //     return handleError(error, 'deleting');
+  //   }
+  // }
+
+  // // Get menu with start date (GET) - No Authentication Required
+  // if (httpMethod === 'GET' && path.endsWith('/menus')) {
+  //   try {
+  //     const { weekStartDate } = queryStringParameters;
+
+  //     if (!weekStartDate) {
+  //       return sendResponse(400, 'weekStartDate query parameter is required.');
+  //     }
+
+  //     const menu = await Menus.findOne({
+  //       weekStartDate: new Date(weekStartDate),
+  //     }).populate('days.dish');
+
+  //     if (!menu) {
+  //       return sendResponse(404, 'Menu not found for the given weekStartDate.');
+  //     }
+
+  //     return sendResponse(200, 'Menu fetched successfully', menu);
+  //   } catch (error) {
+  //     return handleError(error, 'fetching');
+  //   }
+  // }
+
+  // return sendResponse(405, 'Method not allowed.', origin);
 };
